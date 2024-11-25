a7d82fe26a3709d455fcacf2a65d354a9afb2522
akshay <akshay.webnaitra@gmail.com>
2024-11-25 18:20:55 +0530

    category

--- pages/new/category/[category]/index.js
+++ pages/new/category/[category]/index.js
@@ -12,7 +12,7 @@ import {
   fetchCategoriesItems,
   categoriesSelector,
 } from "@/slices/categories";
-import { fetchSources } from "@/slices/sources";
+import { fetchSources, sourcesSelector } from "@/slices/sources";
 import { fetchServerItem, serverItemSelector } from "@/slices/serverItems";
 import { useMediaQuery } from "react-responsive";
 import GreenCaret from "@/components/v2/GreenCaret";
@@ -25,6 +25,7 @@ import Table from "@/components/v2/table";
 import CategorySlider from "@/components/v2/CategorySlider";
 import InfiniteScroll from "react-infinite-scroller";
 import { fetchHomeItems } from "@/slices/homeItems";
+import JawlattLink from "@/components/JawlattLink";
 
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async (context) => {
@@ -58,7 +59,7 @@ const CategoryPage = () => {
     categoriesItemHasError,
     categoriesItemLoading,
   } = useSelector(categoriesSelector);
-
+  const { sources } = useSelector(sourcesSelector);
   const categoryName = router.query.category;
 
   const takeAndSkip = (array, skip, n) => {
@@ -291,7 +292,7 @@ const CategoryPage = () => {
             {/* left side */}
             <div className="col-md-3 order-1 order-md-2">
               <div className="left-side">
-                <div className="card mb-3 jawlatt-card-border rounded-4">
+                {/* <div className="card mb-3 jawlatt-card-border rounded-4">
                   <div className="card-header py-3 pb-0">
                     <h5 className="card-title fw-bold text-end mb-0" dir="ltr">
                       أخبار مصر
@@ -330,6 +331,63 @@ const CategoryPage = () => {
                         </li>
                       ))}
                     </ul>
+                  </div>
+                </div> */}
+                <div className="card mb-3 jawlatt-card-border rounded-4">
+                  <div className="card-header py-3 pb-0">
+                    <h5 className="card-title fw-bold text-end mb-0" dir="ltr">
+                      أخبار مصر
+                      <i className="fa-solid fa-caret-left ms-2  jawlatt-text-red" />
+                    </h5>
+                  </div>
+                  <div className="card-body p-3">
+                    <ul className="list-group ">
+                      {Array.isArray(sources) &&
+                        sources?.slice(0, 7).map((item, index) => (
+                          <li
+                            key={index}
+                            className="list-group-item py-2 px-0 border-0"
+                          >
+                            <div className="d-flex gap-2 align-items-center justify-content-between w-100 ">
+                              <JawlattLink
+                                className="d-flex text-decoration-none"
+                                href={`/new/source/${item?.name}`}
+                              >
+                                <p
+                                  className="m-0 fw-bold text-start"
+                                  style={{
+                                    fontSize: "15px",
+                                    cursor: "pointer",
+                                  }}
+                                >
+                                  <img
+                                    style={{
+                                      width: 20,
+                                      height: 20,
+                                      marginLeft: 6,
+                                    }}
+                                    src={item?.image}
+                                    className="rounded-circle"
+                                  />
+                                  {item?.name}
+                                </p>
+                              </JawlattLink>
+                              <div className="plus">
+                                <img
+                                  src="../../images/Group 1304.png"
+                                  alt="img"
+                                  style={{ width: 20 }}
+                                />
+                              </div>
+                            </div>
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                  <div className="detail-btn mb-3  text-center">
+                    <button className="text-white px-3  border-0 jawlatt-bg-red jawlatt-detail-btn-border fw-medium">
+                      المزيد
+                    </button>
                   </div>
                 </div>
                 <div className="full-img mb-0 mb-lg-3">
